981f58e7aff81ad824b73d1b4693de4375a3d726
Maksim Goncharov <maksim_goncharov_2006@mail.ru>
2024-11-29 12:50:00 +0300

    Complete main interact section widget class

--- frontend/components/MainInteractSection.cpp
+++ frontend/components/MainInteractSection.cpp
@@ -1,13 +1,36 @@
 #include "../headers/components/MainInteractSection.h"
+#include "../headers/utils/coverFunks.h"
 #include <QVBoxLayout>
+#include <QScrollArea>
+#include <QLayoutItem>
+#include <QWidget>
 
 MainInteractSection::MainInteractSection(QWidget *parent) : QWidget(parent) {
-    QWidget* mainWidget = new QWidget(this);
-    mainWidget->setStyleSheet("background: #272727; border-radius: 20px; width: 200px; height: 500px; ");
+    mainWidget = new QWidget();
+    mainLayout = new QVBoxLayout(mainWidget);
+    mainWidget->setStyleSheet("background: #121212; padding: 5px, 0px; border-radius: 5px;");
     mainWidget->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 
-    QVBoxLayout* mainLayout = new QVBoxLayout;
-    mainLayout->addStretch();
+    QScrollArea* scrollArea = new QScrollArea();
+    scrollArea->setWidget(mainWidget);
+    scrollArea->setWidgetResizable(true);
+    scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    scrollArea->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    scrollArea->setStyleSheet(
+        " QScrollArea { padding: 0px; border-radius: 5px; border: none; } "
+        " QScrollBar:vertical { background: #121212; width: 0px; border: none; } "
+        " QScrollBar::handle:vertical { background: #A5A5A5; } "
+    );
 
-    mainWidget->setLayout(mainLayout);
+    this->setLayout(coverWithoutStretchVLayout(scrollArea));
+}
+
+void MainInteractSection::loadPage(QWidget *page) {
+    QLayoutItem *item;
+    while ((item = this->mainLayout->takeAt(0)) != nullptr) {
+        delete item->widget();
+        delete item;
+    }
+
+    this->mainLayout->addWidget(page);
 }
--- frontend/headers/components/MainInteractSection.h
+++ frontend/headers/components/MainInteractSection.h
@@ -1,12 +1,20 @@
 #ifndef MAININTERACTSECTION_H
 #define MAININTERACTSECTION_H
 
+#include <QVBoxLayout>
+#include <QScrollArea>
 #include <QWidget>
 
 class MainInteractSection : public QWidget {
     Q_OBJECT
 public:
     explicit MainInteractSection(QWidget *parent = nullptr);
+    virtual ~MainInteractSection() = default;
+
+    void loadPage(QWidget* page);
+private:
+    QWidget* mainWidget;
+    QVBoxLayout* mainLayout;
 };
 
 #endif // MAININTERACTSECTION_H
