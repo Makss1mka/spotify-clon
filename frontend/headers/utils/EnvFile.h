38b587c477cd8f487520ce5499eaacf93d0e8273
Maksim Goncharov <maksim_goncharov_2006@mail.ru>
2024-12-02 23:58:29 +0300

    Add method for setting value in env file, and rewrite method for getting values from ut

--- frontend/headers/utils/EnvFile.h
+++ frontend/headers/utils/EnvFile.h
@@ -5,7 +5,8 @@
 
 class Env {
 public:
-    static QString get(QString key, QString envFilePath);
+    static QString get(const QString& key);
+    static void set(const QString& key, const QString& value);
 
     Env(const Env&) = delete;
     void operator=(const Env&) = delete;
--- frontend/utils/EnvFile.cpp
+++ frontend/utils/EnvFile.cpp
@@ -6,10 +6,10 @@
 
 Env::Env() {}
 
-QString Env::get(QString key, QString envFilePath) {
-    QFile file(envFilePath);
+QString Env::get(const QString& key) {
+    QFile file("C:/Users/maksi/Desktop/PopuskPapka/C++/KuRsAcH/frontend/.env");
     if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-        qDebug() << "Cannot open env file with path: " << envFilePath;
+        qDebug() << "Cannot open env file with path: " << "C:/Users/maksi/Desktop/PopuskPapka/C++/KuRsAcH/frontend/.env";
         return "";
     }
 
@@ -23,4 +23,39 @@ QString Env::get(QString key, QString envFilePath) {
     }
 
     return "";
+}
+
+void Env::set(const QString& key, const QString& value) {
+    std::map<QString, QString> envMap;
+
+    QFile file("C:/Users/maksi/Desktop/PopuskPapka/C++/KuRsAcH/frontend/.env");
+
+    if (!file.open(QIODevice::ReadWrite | QIODevice::Text)) {
+        qDebug() << "Cannot open env file with path: " << "C:/Users/maksi/Desktop/PopuskPapka/C++/KuRsAcH/frontend/.env";
+        return;
+    }
+
+    QTextStream in(&file);
+    //QList<QList<QString>> lines;
+
+    while (!in.atEnd()) {
+        QString line = in.readLine();
+        if (line.contains("=")) {
+            QList<QString> keyValue = line.split("=");
+            if(keyValue.size() == 2) {
+                envMap[keyValue[0]] = keyValue[1];
+            }
+        }
+    }
+
+    envMap[key] = value;
+
+    file.resize(0);
+    QTextStream out(&file);
+
+    for(const auto& pair : envMap) {
+        out << pair.first << "=" << pair.second << "\n";
+    }
+
+    file.close();
 }
